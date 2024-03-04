fede59213a12360b41d93b7e72718994b6d7367d
LukacsBotond <57891898+LukacsBotond@users.noreply.github.com>
2024-03-04 17:49:37 +0200

    hsync and hsysnc timed

--- pio/vga/vga.cpp
+++ pio/vga/vga.cpp
@@ -3,19 +3,18 @@
 VGA::VGA(PIO vSync, uint vSyncSM, PIO hSync, uint hSyncSM)
 {
     uint offsetHsync = pio_add_program(vSync, &vga_hsync_program);
-    //uint offsetVsync = pio_add_program(hSync, &vga_hsync_program);
+    uint offsetVsync = pio_add_program(hSync, &vga_vsync_program);
     printf("Hsync loaded%d \n", offsetHsync);
-    //printf("Vsync loaded%d \n", offsetHsync);
+    printf("Vsync loaded%d \n", offsetHsync);
 
     // init
     vga_hsync_program_init(hSync, hSyncSM, offsetHsync, HSYNC);
-    //vga_vsync_program_init(vSync, vSyncSM, offsetVsync, VSYNC);
-    pio_sm_put_blocking(hSync, hSyncSM, (640 + 16 + 48) / 16); //running a 16x extra divider
-    //pio_sm_put_blocking(vSync, vSyncSM, 480/2);
+    vga_vsync_program_init(vSync, vSyncSM, offsetVsync, VSYNC);
+    pio_sm_put_blocking(hSync, hSyncSM, (640 + 16 + 48) / 16); // running a 16x extra divider
+    pio_sm_put_blocking(vSync, vSyncSM, ((640 + 16 + 48) / 16) * (480 + 10 + 103));
 
     pio_sm_set_enabled(hSync, hSyncSM, true);
-    //pio_sm_set_enabled(vSync, vSyncSM, true);
-    
+    pio_sm_set_enabled(vSync, vSyncSM, true);
 }
 
 VGA::~VGA()
--- pio/vga/vvga.h
+++ pio/vga/vvga.h
@@ -13,21 +13,26 @@
 // --------- //
 
 #define vga_vsync_wrap_target 1
-#define vga_vsync_wrap 3
+#define vga_vsync_wrap 8
 
 static const uint16_t vga_vsync_program_instructions[] = {
     0x6040, //  0: out    y, 32           side 0     
             //     .wrap_target
-    0xaf22, //  1: mov    x, y            side 0 [15]
-    0x1042, //  2: jmp    x--, 2          side 1     
-    0xaf42, //  3: nop                    side 0 [15]
+    0xb022, //  1: mov    x, y            side 1     
+    0x2006, //  2: wait   0 gpio, 6       side 0     
+    0x2086, //  3: wait   1 gpio, 6       side 0     
+    0x2006, //  4: wait   0 gpio, 6       side 0     
+    0x2086, //  5: wait   1 gpio, 6       side 0     
+    0x2006, //  6: wait   0 gpio, 6       side 0     
+    0x2086, //  7: wait   1 gpio, 6       side 0     
+    0x1048, //  8: jmp    x--, 8          side 1     
             //     .wrap
 };
 
 #if !PICO_NO_HARDWARE
 static const struct pio_program vga_vsync_program = {
     .instructions = vga_vsync_program_instructions,
-    .length = 4,
+    .length = 9,
     .origin = -1,
 };
 
@@ -47,7 +52,7 @@ static inline void vga_vsync_program_init(PIO pio, uint stma, uint offset, uint 
     sm_config_set_set_pins(&c, vsync, 1);
     sm_config_set_out_shift(&c,true,true,8);
     // clock
-    sm_config_set_clkdiv_int_frac(&c, 0x1000,0);
+    sm_config_set_clkdiv_int_frac(&c, 178,0);
     pio_sm_init(pio, stma, offset, &c);
     pio_sm_set_enabled(pio, stma, true);
 }
